97971d0822653a9557154bb049840856d6189f5f
david <david@lexprogram.com>
2024-09-09 08:11:24 +0200

    Metido el navbar en un componente

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <v-responsive>
-    <v-app :theme="theme">
-      <v-app-bar class="px-3" :absolute="true">
-        <nav v-if="!userStore.loadingSession">
-          <router-link to="/" v-if="userStore.userData">Home</router-link> |
-          <router-link to="/login" v-if="!userStore.userData">Login</router-link> |
-          <router-link to="/register" v-if="!userStore.userData">Register</router-link> |
-          <button @click="userStore.logoutUser" v-if="userStore.userData">Logout</button>
-        </nav>
-        <v-spacer></v-spacer>
-
-        <v-btn :prepend-icon="theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny'"
-          text="Toggle Theme" slim @click="onClick"></v-btn>
-      </v-app-bar>
-
+    <v-app>
+      <Navbar />
       <v-main>
         <v-container>
           <router-view></router-view>
@@ -24,13 +12,8 @@
 </template>
 
 <script setup>
-import { useUserStore } from './stores/user'
-import { useTheme } from 'vuetify'
+import { useTheme } from "vuetify";
+import Navbar from "@/components/Navbar.vue";
 
-const userStore = useUserStore()
-const theme = useTheme()
-
-function onClick() {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-}
+const theme = useTheme();
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar class="px-3" :absolute="true">
+    <v-img
+      class="mx-2"
+      :src="logo"
+      max-height="30"
+      max-width="30"
+      contain
+    ></v-img>
+    <!--
+    <nav v-if="!userStore.loadingSession">
+      <router-link to="/" v-if="userStore.userData">Home</router-link> |
+      <router-link to="/login" v-if="!userStore.userData">Login</router-link> |
+      <router-link to="/register" v-if="!userStore.userData"
+        >Register</router-link
+      >
+      |
+      <button @click="userStore.logoutUser" v-if="userStore.userData">
+        Logout
+      </button>
+    </nav>
+-->
+    <v-spacer></v-spacer>
+
+    <v-btn
+      :prepend-icon="
+        theme.global.current.value.dark
+          ? 'mdi-weather-night'
+          : 'mdi-weather-sunny'
+      "
+      text="Toggle Theme"
+      slim
+      @click="onClick"
+    ></v-btn>
+    <v-btn icon="mdi-login" to="/login"></v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { useUserStore } from "@/stores/user";
+import { useTheme } from "vuetify";
+import logo from "@/assets/logo.png";
+
+const userStore = useUserStore();
+const theme = useTheme();
+
+function onClick() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
