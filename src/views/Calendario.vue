bc50a9ffcdaf29a3fa384bf97295a41ef6dbf217
david <david@lexprogram.com>
2024-09-20 09:14:17 +0200

    Corregido problema en la visualización de eventos tras eliminación

--- src/views/Calendario.vue
+++ src/views/Calendario.vue
@@ -11,12 +11,26 @@
   </v-row>
   <v-row>
     <v-col class="flex-grow-0 flex-shrink-0">
-      <v-date-picker :weekdays="[1, 2, 3, 4, 5, 6, 0]" first-day-of-week="1" location="es eu" v-model="selectedDate"
-        show-adjacent-months ref="datePicker" elevation="4"></v-date-picker>
-      <v-btn color="green-darken-1" prepend-icon="mdi-plus" class="mt-3" variant="outlined" block @click="
-        eventType = 'period';
-      isDialogOpen = true;
-      ">
+      <v-date-picker
+        :weekdays="[1, 2, 3, 4, 5, 6, 0]"
+        first-day-of-week="1"
+        location="es eu"
+        v-model="selectedDate"
+        show-adjacent-months
+        ref="datePicker"
+        elevation="4"
+      ></v-date-picker>
+      <v-btn
+        color="green-darken-1"
+        prepend-icon="mdi-plus"
+        class="mt-3"
+        variant="outlined"
+        block
+        @click="
+          eventType = 'period';
+          isDialogOpen = true;
+        "
+      >
         Crear evento periodico
       </v-btn>
     </v-col>
@@ -24,16 +38,30 @@
       <v-row>
         <v-col cols="12" class="d-flex justify-space-between align-center">
           <h3>Todos los eventos</h3>
-          <v-btn variant="outlined" prepend-icon="mdi-plus" color="green-darken-1" @click="
-            eventType = 'day';
-          isDialogOpen = true;
-          ">
+          <v-btn
+            variant="outlined"
+            prepend-icon="mdi-plus"
+            color="green-darken-1"
+            @click="
+              eventType = 'day';
+              isDialogOpen = true;
+            "
+          >
             Crear evento para {{ formatedDate }}
           </v-btn>
         </v-col>
-        <v-col lg="3" md="6" sm="12" v-for="(event, n) in databaseStore.eventos" :key="n">
-          <EventoCard :evento="event" :equipos="getEquipoDataById(event.equipos)"
-            @deleteEvent="showDeleteDialog(event)" />
+        <v-col
+          lg="3"
+          md="6"
+          sm="12"
+          v-for="event in databaseStore.eventos"
+          :key="event.id"
+        >
+          <EventoCard
+            :evento="event"
+            :equipos="getEquipoDataById(event.equipos)"
+            @deleteEvent="showDeleteDialog(event)"
+          />
         </v-col>
       </v-row>
     </v-col>
@@ -41,22 +69,39 @@
   <v-dialog v-model="deleteEventDialog" max-width="500">
     <v-card title="Eliminar evento?" prepend-icon="mdi-calendar-remove-outline">
       <v-divider></v-divider>
-      <EventoCard id="card-delete-preview" :evento="eventToDelete"
-        :equipos="getEquipoDataById(eventToDelete?.equipos)" />
+      <EventoCard
+        id="card-delete-preview"
+        :evento="eventToDelete"
+        :equipos="getEquipoDataById(eventToDelete?.equipos)"
+      />
       <v-divider></v-divider>
       <v-card-actions class="d-flex justify-space-between">
-        <v-btn @click="deleteEventDialog = false" prepend-icon="mdi-close" variant="outlined">
+        <v-btn
+          @click="deleteEventDialog = false"
+          prepend-icon="mdi-close"
+          variant="outlined"
+        >
           Cancelar
         </v-btn>
-        <v-btn prepend-icon="mdi-trash-can-outline" color="red" variant="outlined" @click="deleteEventAction()"
-          :loading="databaseStore.loadingDeleteDoc">
+        <v-btn
+          prepend-icon="mdi-trash-can-outline"
+          color="red"
+          variant="outlined"
+          @click="deleteEventAction()"
+          :loading="databaseStore.loadingDeleteDoc"
+        >
           Eliminar
         </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
-  <EventoFormModal :isOpen="isDialogOpen" actionType="crear" :eventType="eventType" :eventDay="formatedDate"
-    @closeDialog="isDialogOpen = false" />
+  <EventoFormModal
+    :isOpen="isDialogOpen"
+    actionType="crear"
+    :eventType="eventType"
+    :eventDay="formatedDate"
+    @closeDialog="isDialogOpen = false"
+  />
 </template>
 
 <script setup>
@@ -75,19 +120,20 @@ const isDialogOpen = ref(false);
 const datePicker = ref(null);
 const selectedDate = ref(new Date());
 const formatedDate = ref(date.format(selectedDate, "keyboardDate"));
-const deleteEventDialog = ref(false)
-const eventToDelete = ref(null)
+const deleteEventDialog = ref(false);
+const eventToDelete = ref(null);
 
 const showDeleteDialog = (eventData) => {
-  deleteEventDialog.value = true
-  eventToDelete.value = eventData
-}
+  deleteEventDialog.value = true;
+  eventToDelete.value = eventData;
+};
 
 const deleteEventAction = () => {
-  databaseStore.deleteEvent(eventToDelete.value.id)
-  deleteEventDialog.value = false
-  eventToDelete.value = null
-}
+  databaseStore.deleteEvent(eventToDelete.value.id);
+  deleteEventDialog.value = false;
+  eventToDelete.value = null;
+  console.log(databaseStore.eventos);
+};
 
 const getEquipoDataById = (ids) => {
   if (!ids) return [];
@@ -137,6 +183,6 @@ watch(selectedDate, (newVal) => {
 
 <style>
 #card-delete-preview {
-  scale: .8;
+  scale: 0.8;
 }
 </style>
