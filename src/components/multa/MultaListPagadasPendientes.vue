29c0c1ec80aeb66cce28b81edbfaa25e3120b4a1
david <david@lexprogram.com>
2024-10-04 14:39:56 +0200

    Inicio de la representación de multas

--- src/components/multa/MultaListPagadasPendientes.vue
+++ src/components/multa/MultaListPagadasPendientes.vue
@@ -1,19 +1,58 @@
 <template>
   <v-row>
-    <v-col cols="12" md="5">
-      <h3 class="text-center">
+    <v-col cols="12" md="6">
+      <h3 class="text-center mb-3">
         <v-icon icon="mdi-invoice-clock-outline" size="small"></v-icon>
         Multas pendientes
       </h3>
-      <pre>
-      {{ multaStore.multasGroupedByJugador }}
-      </pre>
+      <v-card
+        v-for="jugadorMultas in multaStore.multasGroupedByJugador"
+        :key="jugadorMultas.id"
+        :title="`${jugadorMultas.mote} (${jugadorMultas.dorsal})`"
+        :subtitle="`${jugadorMultas.nombre} ${jugadorMultas.apellidos}`"
+        variant="outlined"
+      >
+        <template v-slot:append>
+          <div class="d-flex flex-column align-center pe-n2">
+            <span> Total </span>
+            <v-sheet rounded class="px-2 py-1">
+              <span class="d-flex align-center">
+                {{ totalPagoJugador(jugadorMultas.multas) }}
+                <v-icon icon="mdi-currency-eur" size="x-small"></v-icon>
+              </span>
+            </v-sheet>
+          </div>
+        </template>
+        <v-card-text>
+          <v-list lines="one" bg-color="transparent" nav class="pa-0">
+            <v-list-item v-for="multa in jugadorMultas.multas" :key="multa.id">
+              <template v-slot:prepend="{ isActive }">
+                <v-list-item-action start>
+                  <v-checkbox
+                    v-model="formState.selectedEventIDs"
+                    :value="multa.pagado"
+                  ></v-checkbox>
+                </v-list-item-action>
+              </template>
+              <v-list-item-title
+                class="text-body-1 d-flex justify-space-between align-center pt-2 pb-1"
+              >
+                <div class="d-flex flex-column">
+                  <span> {{ multa.concepto }} </span>
+                  <span class="text-caption text-grey"> 00-00-0000 </span>
+                </div>
+                <span class="d-flex">
+                  {{ multa.valor }}
+                  <v-icon icon="mdi-currency-eur" size="x-small"></v-icon>
+                </span>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-card>
     </v-col>
-    <v-col cols="12" md="2" class="text-center">
-      <v-divider class="h-100 border-opacity-100" vertical></v-divider>
-    </v-col>
-    <v-col cols="12" md="5">
-      <h3 class="text-center">
+    <v-col cols="12" md="6">
+      <h3 class="text-center" mb-3>
         <v-icon icon="mdi-invoice-check-outline" size="small"></v-icon>
         Multas pagadas
       </h3>
@@ -22,9 +61,19 @@
 </template>
 
 <script setup>
+import { reactive } from "vue";
 import { useMultaStore } from "../../stores/multa";
 
 const multaStore = useMultaStore();
 multaStore.getMultasJugador();
-console.log(multaStore.multasGroupedByJugador);
+
+const formState = reactive({
+  selectedEventIDs: [],
+});
+
+const totalPagoJugador = (multasJugador) => {
+  let result = 0;
+  multasJugador.map((m) => (result += parseInt(m.valor)));
+  return result;
+};
 </script>
